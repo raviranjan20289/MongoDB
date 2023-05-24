54411afb221b2476c34a48831fd73eb6db459416
raviranjan202 <mailraviranjan28@gmail.com>
2023-05-25 02:40:45 +0530

    ordered Placed

--- controllers/shop.js
+++ controllers/shop.js
@@ -84,25 +84,25 @@ exports.postCartDeleteProduct = (req, res, next) => {
       })
       .catch(err => console.log(err));
   };
-exports.postOrder = (req, res, next) => {
-  let fetchedCart;
-  req.user
-   .addOrder()
-    .then(result => {
-      res.redirect('/orders');
-    })
-    .catch(err => console.log(err));
-};
+  exports.postOrder = (req, res, next) => {
+    let fetchedCart;
+    req.user
+     .addOrder()
+      .then(result => {
+        res.redirect('/orders');
+      })
+      .catch(err => console.log(err));
+  };
 
-exports.getOrders = (req, res, next) => {
-  req.user
-    .getOrders()
-    .then(orders => {
-      res.render('shop/orders', {
-        path: '/orders',
-        pageTitle: 'Your Orders',
-        orders: orders
-      });
-    })
-    .catch(err => console.log(err));
-};
+  exports.getOrders = (req, res, next) => {
+    req.user
+      .getOrders()
+      .then(orders => {
+        res.render('shop/orders', {
+          path: '/orders',
+          pageTitle: 'Your Orders',
+          orders: orders
+        });
+      })
+      .catch(err => console.log(err));
+  };
