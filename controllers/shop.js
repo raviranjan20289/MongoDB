95efd9923e0376e5358f6beae3ad07138900e78b
raviranjan202 <mailraviranjan28@gmail.com>
2023-05-25 04:12:19 +0530

    placed order

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,40 +1,32 @@
 const Product = require('../models/product');
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
-    .then(products => {
-      res.render('shop/product-list', {
-        prods: products,
-        pageTitle: 'All Products',
-        path: '/products'
+    Product.find()
+      .then(products => {
+        res.render('shop/product-list', {
+          prods: products,
+          pageTitle: 'All Products',
+          path: '/products'
+        });
+      })
+      .catch(err => {
+        console.log(err);
       });
-    })
-    .catch(err => {
-      console.log(err);
-    });
-};
+  };
+  
 
-exports.getProduct = (req, res, next) => {
-  const prodId = req.params.productId;
-  // Product.findAll({ where: { id: prodId } })
-  //   .then(products => {
-  //     res.render('shop/product-detail', {
-  //       product: products[0],
-  //       pageTitle: products[0].title,
-  //       path: '/products'
-  //     });
-  //   })
-  //   .catch(err => console.log(err));
-  Product.findById(prodId)
-    .then(product => {
-      res.render('shop/product-detail', {
-        product: product,
-        pageTitle: product.title,
-        path: '/products'
-      });
-    })
-    .catch(err => console.log(err));
-};
+  exports.getProduct = (req, res, next) => {
+    const prodId = req.params.productId;
+    Product.findById(prodId)
+      .then(product => {
+        res.render('shop/product-detail', {
+          product: product,
+          pageTitle: product.title,
+          path: '/products'
+        });
+      })
+      .catch(err => console.log(err));
+  };
 
 exports.getIndex = (req, res, next) => {
   Product.fetchAll()
@@ -96,8 +88,7 @@ exports.postCart = (req, res, next) => {
   };
 
   exports.getOrders = (req, res, next) => {
-    req.user
-      .getOrders()
+    Order.find({'user.userId':req.user._id})
       .then(orders => {
         res.render('shop/orders', {
           path: '/orders',
